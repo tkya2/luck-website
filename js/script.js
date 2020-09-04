9b616a420299fee5ce42dfd9e34f5b6ece271678
tkya2 <t.koyama0123@gmail.com>
2020-09-04 23:07:40 +0900

    push-9.4

--- js/script.js
+++ js/script.js
@@ -152,3 +152,26 @@ document.getElementById( "btn-backtotop" ).addEventListener('click', function(e)
 	});
 	return false;
 });
+
+
+// personnel---top部分
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters ,ml7');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 70 * i     //動くスピード
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,  //切り替えスピード
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  
