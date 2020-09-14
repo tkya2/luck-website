c2a071a51190e53544b2c79fd29399faebde63b3
tkya2 <t.koyama0123@gmail.com>
2020-09-14 14:01:19 +0900

    commit

---
+++ js/scroll.js
@@ -0,0 +1,34 @@
+ScrollReveal().reveal('.main-contents_left', {
+   delay: 200, 
+   distance: '200px', 
+   origin: 'left' 
+});
+ScrollReveal().reveal('.main-contents_right', {
+  delay: 200, 
+  distance: '200px', 
+  origin: 'right' 
+});
+ScrollReveal().reveal('.main-contents_company-image', {
+  delay: 200, 
+  distance: '200px', 
+  origin: 'right' 
+});
+ScrollReveal().reveal('.btn-wrapper', {
+  delay: 500, 
+  distance: '100px', 
+  duration: 1500, //アニメーションの完了にかかる時間
+  scale:2, //アニメーションの最初大きさ(倍)
+});
+
+ScrollReveal().reveal('.detail-description', {
+  delay: 200, 
+  distance: '100px', 
+  duration: 1500, //アニメーションの完了にかかる時間
+});
+
+
+ScrollReveal().reveal('.personnel_writer img , h2', {
+  delay: 200, 
+  distance: '100px', 
+  duration: 1500, //アニメーションの完了にかかる時間
+});
